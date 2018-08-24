8a7a82e12697ab3478b9765983fd8a614e232382
Vincent Sellier <vsellier@exoplatform.com>
2018-08-24 19:37:22 +0200

    System audit

---
+++ audit/_functions.sh
@@ -0,0 +1,27 @@
+#/usr/bin/env bash
+
+# Detect OS Type
+if [ ! -e /etc/os-release ]; then 
+  echo "Unable to detect os type via /etc/os-release file"
+  exit 1
+fi
+
+source /etc/os-release
+
+if [ -z "$ID" ]; then
+  echo "ID variable not specified on the /etc/os-release file"
+  exit 1
+fi
+
+OS="${ID}"
+OS_TYPE="$(echo "${ID_LIKE:-$ID}" | awk '{print $1}')"   # for centos ID_LIKE = "rhel fedora)"
+
+echo "OS detected : ${OS}"
+echo "OS type : ${OS_TYPE}"
+
+USER=$(whoami)
+
+if [ "${USER}" != "root" ]; then
+  echo "Running as ${USER}, sudo will be used when needed"
+  SUDO="sudo "
+fi
---
+++ audit/system.sh
@@ -0,0 +1,79 @@
+#/usr/bin/env bash
+
+source _functions.sh
+
+OUTPUT_DIR=/tmp/exo-audit/system
+
+mkdir -p ${OUTPUT_DIR}
+pushd ${OUTPUT_DIR}
+
+date +%Y%m%d-%H%M > ${OUTPUT_DIR}/0_REPORT_DATE
+
+echo "Generating hostname..."
+hostname > hostname.txt
+
+echo "Getting distribution details..."
+cat /etc/*-release > distribution.txt
+uname -a > kernel.txt
+
+echo "Checking authorized keys..."
+cat ~/.ssh/authorized_keys > ssh_accesses.txt
+
+echo "Checking memory details "
+free > memory.txt
+cat /proc/swaps > swap.txt
+
+echo "Checking network interfaces..."
+ip a > network.txt
+ss -l -n -t > network_open_ports.txt
+
+echo "Checking cpus..."
+cp /proc/cpuinfo cpuinfo.txt
+
+echo "Capturing running processes..."
+ps -ef > processes.txt
+
+echo "Checking raid configuration..."
+if [ -e '/proc/mdstat' ]; then
+  cat /proc/mdstat > md.txt
+else 
+  echo "No software raids detected (no /proc/mdstat file)" > md.txt
+fi
+
+echo "Checking lvm installation..."
+if [ -e "/sbin/lvdisplay" ]; then
+  # TODO use sudo
+  pvdisplay > lvm.txt
+  vgdisplay >> lvm.txt
+  lvdisplay >> lvm.txt
+else 
+  echo "LVM not detected"
+  echo "" > lvm.notinstalled
+fi
+
+echo "Checking mount point..."
+cp /etc/fstab fstab
+mount > mounts.txt
+
+echo "Checking partition spaces..."
+df -h > disk_space.txt
+
+echo "Checking kernel configuration..."
+/sbin/sysctl -a > sysctl.lst
+
+if [ -e /etc/monitrc ]; then
+  tar cvjf monit.tar.gz /etc/monitrc /etc/monit.d
+fi
+
+echo "Checking backup manager usage..."
+if [ -e backup-manager.conf ]; then
+  echo "Backup manager detected"
+  cp -v /etc/backup-manager.conf .
+else
+  echo "Backup manager not detected"
+  echo "" > backup-manager.notused
+fi
+
+echo ""
+echo "Result are stored on $(pwd) directory"
+popd

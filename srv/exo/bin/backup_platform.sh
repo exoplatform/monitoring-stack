5ad95d3296be99b9e61b691c07982e399521ac17
Vincent Sellier <vsellier@exoplatform.com>
2020-04-14 19:16:35 +0200

    allow to backup locally without ssh usage

--- srv/exo/bin/backup_platform.sh
+++ srv/exo/bin/backup_platform.sh
@@ -17,28 +17,30 @@ DOWNTIME_START_TIME=$(date +%s)
 
 BACKUP_DATE=$(date "+%Y-%m-%d-%H%M%S")
 
+SSH_COMMAND="$(getSSHCommand ${EXO_PLF_SERVER} ${EXO_USER})"
+
 # Stop it
-ssh ${EXO_USER}@${EXO_PLF_SERVER} ${SCRIPT_DIR}/_stopeXo.sh
+${SSH_COMMAND} ${SCRIPT_DIR}/_stopeXo.sh
 
 # Dump data
-ssh ${EXO_USER}@${EXO_PLF_SERVER} ${SCRIPT_DIR}/_dumpData.sh ${BACKUP_DATE}
+${SSH_COMMAND} ${SCRIPT_DIR}/_dumpData.sh ${BACKUP_DATE}
 
 # Dump database
-ssh ${EXO_USER}@${EXO_DB_SERVER} ${SCRIPT_DIR}/_dumpMysqlDatabase.sh ${BACKUP_DATE}
+${SSH_COMMAND} ${SCRIPT_DIR}/_dumpMysqlDatabase.sh ${BACKUP_DATE}
 
 # Dump MongoDB
-ssh ${EXO_USER}@${EXO_MONGO_SERVER} ${SCRIPT_DIR}/_dumpMongoDb.sh ${BACKUP_DATE}
+${SSH_COMMAND} ${SCRIPT_DIR}/_dumpMongoDb.sh ${BACKUP_DATE}
 
 # Dump Elastic
-ssh ${EXO_USER}@${EXO_ES_SERVER} ${SCRIPT_DIR}/_stopElasticsearch.sh
-ssh ${EXO_USER}@${EXO_ES_SERVER} ${SCRIPT_DIR}/_dumpElasticsearch.sh ${BACKUP_DATE}
-ssh ${EXO_USER}@${EXO_ES_SERVER} ${SCRIPT_DIR}/_startElasticsearch.sh
+${SSH_COMMAND} ${SCRIPT_DIR}/_stopElasticsearch.sh
+${SSH_COMMAND} ${SCRIPT_DIR}/_dumpElasticsearch.sh ${BACKUP_DATE}
+${SSH_COMMAND} ${SCRIPT_DIR}/_startElasticsearch.sh
 
 # Start it
-ssh ${EXO_USER}@${EXO_PLF_SERVER} ${SCRIPT_DIR}/_starteXo.sh
+${SSH_COMMAND} ${SCRIPT_DIR}/_starteXo.sh
 
 DOWNTIME_END_TIME=$(date +%s)
-if [ ${REMOTE_BACKUP} ]; then
+if [ "${REMOTE_BACKUP}" == true ]; then
   rsync -av ${EXO_USER}@${EXO_PLF_SERVER}:${BACKUP_WORKING_DIR}/tmp_data/* ${BACKUP_DIR}
   rsync -av ${EXO_USER}@${EXO_DB_SERVER}:${BACKUP_WORKING_DIR}/tmp_db/* ${BACKUP_DIR}
   rsync -av ${EXO_USER}@${EXO_MONGO_SERVER}:${BACKUP_WORKING_DIR}/tmp_mongo/* ${BACKUP_DIR}
@@ -55,7 +57,7 @@ ${SCRIPT_DIR}/_cleanBackups.sh
 SCRIPT_END_TIME=$(date +%s)
 
 echo "[INFO] ======================================="
-echo "[INFO] = Backup ended -" $(date)
+echo "[INFO] = Backup ended - $(date)"
 echo "[INFO] =--------------------------------------"
 display_delay "= -> Process duration" $SCRIPT_START_TIME $SCRIPT_END_TIME
 echo "[INFO] =--------------------------------------"

a6693b45c40c9a026b4d8a1809011d373705ad57
Vincent Sellier <vsellier@exoplatform.com>
2020-04-14 19:21:18 +0200

    allow to restore locally without using ssh

--- srv/exo/bin/restore_platform.sh
+++ srv/exo/bin/restore_platform.sh
@@ -19,9 +19,11 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
+SSH_COMMAND="$(getSSHCommand ${EXO_PLF_SERVER} ${EXO_USER})"
+
 BACKUP_DATE=$1
 
-if ${REMOTE_BACKUP}; then
+if [ "${REMOTE_BACKUP}" == "true" ] ; then
   rsync -av ${BACKUP_DIR}/${PLF_NAME}-data-${BACKUP_DATE}.tar.bz2 ${EXO_USER}@${EXO_PLF_SERVER}:${BACKUP_WORKING_DIR}/tmp_data/ 
   rsync -av ${BACKUP_DIR}/${PLF_NAME}-db-${BACKUP_DATE}.sql.bz2 ${EXO_USER}@${EXO_DB_SERVER}:${BACKUP_WORKING_DIR}/tmp_db/
   rsync -av ${BACKUP_DIR}/${PLF_NAME}-${CHAT_DATABASE}-${BACKUP_DATE}.tar.bz2 ${EXO_USER}@${EXO_MONGO_SERVER}:${BACKUP_WORKING_DIR}/tmp_mongo/
@@ -36,30 +38,30 @@ fi
 DOWNTIME_START_TIME=$(date +%s)
 
 # Stop it
-ssh ${EXO_USER}@${EXO_PLF_SERVER} ${SCRIPT_DIR}/_stopeXo.sh
+${SSH_COMMAND} ${SCRIPT_DIR}/_stopeXo.sh
 
 # restore data
-ssh ${EXO_USER}@${EXO_PLF_SERVER} ${SCRIPT_DIR}/_restoreData.sh ${BACKUP_DATE}
+${SSH_COMMAND} ${SCRIPT_DIR}/_restoreData.sh ${BACKUP_DATE}
 
 # Dump database
-ssh ${EXO_USER}@${EXO_DB_SERVER} ${SCRIPT_DIR}/_restoreMysqlDatabase.sh ${BACKUP_DATE}
+${SSH_COMMAND} ${SCRIPT_DIR}/_restoreMysqlDatabase.sh ${BACKUP_DATE}
 
 # Dump MongoDB
-ssh ${EXO_USER}@${EXO_MONGO_SERVER} ${SCRIPT_DIR}/_restoreMongoDb.sh ${BACKUP_DATE}
+${SSH_COMMAND} ${SCRIPT_DIR}/_restoreMongoDb.sh ${BACKUP_DATE}
 
 # Dump Elastic
-ssh ${EXO_USER}@${EXO_ES_SERVER} ${SCRIPT_DIR}/_stopElasticsearch.sh
-ssh ${EXO_USER}@${EXO_ES_SERVER} ${SCRIPT_DIR}/_restoreElasticsearch.sh ${BACKUP_DATE}
-ssh ${EXO_USER}@${EXO_ES_SERVER} ${SCRIPT_DIR}/_startElasticsearch.sh
+${SSH_COMMAND} ${SCRIPT_DIR}/_stopElasticsearch.sh
+${SSH_COMMAND} ${SCRIPT_DIR}/_restoreElasticsearch.sh ${BACKUP_DATE}
+${SSH_COMMAND} ${SCRIPT_DIR}/_startElasticsearch.sh
 
 # Start it
-ssh ${EXO_USER}@${EXO_PLF_SERVER} ${SCRIPT_DIR}/_starteXo.sh
+${SSH_COMMAND} ${SCRIPT_DIR}/_starteXo.sh
 
 DOWNTIME_END_TIME=$(date +%s)
 SCRIPT_END_TIME=$(date +%s)
 
 echo "[INFO] ======================================="
-echo "[INFO] = Backup ended -" $(date)
+echo "[INFO] = Backup ended - $(date)"
 echo "[INFO] =--------------------------------------"
 display_delay "= -> Process duration" $SCRIPT_START_TIME $SCRIPT_END_TIME
 echo "[INFO] =--------------------------------------"
